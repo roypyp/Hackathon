22f530bfc2b04d1dbc9f982bbd015347256c8ffa
roypyp <roypyp@gmail.com>
2020-12-27 18:01:16 +0200

    dfsdf

--- Client.py
+++ Client.py
@@ -1,13 +1,28 @@
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 1234))
+print("Client started, listening for offer requests....")
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+s.bind(('',13117))
 
-full_msg = ''
-while True:
-    msg = s.recv(8)
-    if len(msg) <= 0:
-        break
-    full_msg += msg.decode("utf-8")
 
-print(full_msg)
+try:
+    message, address = s.recvfrom(10104)
+    print( "Received offer from ",address[0] ,",attempting to connect...")
+except (KeyboardInterrupt,SystemExit):
+    raise
+
+
+
+
+
+# s.connect((socket.gethostname(), 1234))
+# full_msg = ''
+# while True:
+#     msg = s.recv(8)
+#     if len(msg) <= 0:
+#         break
+#     full_msg += msg.decode("utf-8")
+
+# print(full_msg)
--- Server.py
+++ Server.py
@@ -1,12 +1,26 @@
-import socket
+from socket import *
+import time
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((socket.gethostname(), 1234))
-s.listen(5)
+print("Server started, listening on IP address 172.1.0.36")
 
+s = socket(AF_INET, SOCK_DGRAM)
+
+
+msg = bytes(gethostbyname(gethostname()),"utf-8")
 while True:
-    # now our endpoint knows about the OTHER endpoint.
-    clientsocket, address = s.accept()
-    print(f"Connection from {address} has been established.")
-    clientsocket.send(bytes("Hey there!!! sdfdsf","utf-8"))
-    clientsocket.close()
+    #s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+    s.sendto(msg, ('<broadcast>', 13117))
+    time.sleep(1)
+
+
+
+# s.bind((socket.gethostname(), 1234))
+# s.listen(5)
+# while True:
+    
+#     # now our endpoint knows about the OTHER endpoint.
+#     clientsocket, address = s.accept()
+#     print(f"Connection from {address} has been established.")
+#     clientsocket.send(bytes("Hey there!!! sdfdsf","utf-8"))
+#     clientsocket.close()
