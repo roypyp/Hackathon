43194790b26ecf78a368c6fb2634bc994e95a4bc
roypyp <roypyp@gmail.com>
2020-12-27 22:07:11 +0200

    fdsfdsfsd

--- Client.py
+++ Client.py
@@ -2,12 +2,12 @@ import socket
 
 print("Client started, listening for offer requests....")
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-#s2= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s2= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-s.bind(('',13117))
+s.bind(('',13217))
 
-teamname="isengard"
+teamname="Isengard"
 cha=b'\xfe\xed\xbe\xef\x02'
 
 try:
@@ -15,14 +15,15 @@ try:
     print( "Received offer from ",address[0] ,",attempting to connect...")
     if(message[0:5]!=cha[0:5]):
         print("wrong broadcast")
-    port_num=int(hex(message[5])[2:]+hex(message[6])[2:])
+    port_num=int(hex(message[5])[2:]+hex(message[6])[2:],16)
     print(port_num)
+    s2.connect(("172.1.0."+address[0].split(".")[3], port_num))
+    s2.sendall(bytes(teamname,"utf-8"))
     
 except (KeyboardInterrupt,SystemExit):
     raise
 
-#s.connect((address[0], port_num))
-#s.sendall(bytes(teamname,"utf-8"))
+
 
 
 # s.connect((socket.gethostname(), 1234))
--- Server.py
+++ Server.py
@@ -1,26 +1,50 @@
 from socket import *
 import time
-import thread
+import _thread
+import select
 
-print("Server started, listening on IP address 172.1.0.36")
-
-s = socket(AF_INET, SOCK_DGRAM)
-#s2 = socket(AF_INET, SOCK_STREAM)
-#s2.bind((gethostname(), 2513))
-#s2.listen()
-
-msg = b'\xfe\xed\xbe\xef\x02\x25\x13'
-#bytes(gethostbyname(gethostname()),"utf-8")
-while True:
-    #s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+teamsdict={}
+def listenT(s):
+    global teamsdict
+    print(startTime)
+    while True:
+        readable, writeable, erro = select.select([s],[],[],startTime+10-time.time())
+        if len(readable)<=0:
+            break
+        conn, addr = s.accept()
+        print("hey")
+        data = conn.recv(1024)
+        teamsdict[addr]=[conn,data.decode("utf-8")]
+        print(data.decode("utf-8"))
+        #_thread.start_new_thread(listenT,())
+def broadCast():
+    print("Server started, listening on IP address ", "172.1.0."+gethostbyname(gethostname()).split(".")[3])
+    s = socket(AF_INET, SOCK_DGRAM)
     s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
-    s.sendto(msg, ('<broadcast>', 13117))
-    time.sleep(1)
-
+    msg = b'\xfe\xed\xbe\xef\x02\x25\x13'
     
-    #conn, addr = s2.accept()
-    #data = conn.recv(1024)
-    #print(data.decode("utf-8"))
+    for i in range(10):
+    #s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        s.sendto(msg, ('<broadcast>', 13217))
+        time.sleep(1)
+
+def KSBR():
+    global teamsdict
+
+
+s = socket(AF_INET, SOCK_STREAM)
+s.bind(("172.1.0."+gethostbyname(gethostname()).split(".")[3], 9491))
+s.listen()
+
+while True:
+    _thread.start_new_thread(broadCast,())
+    _thread.start_new_thread(listenT,(s,))
+    time.sleep(11)
+
+
+
+
+
 
 # s.bind((socket.gethostname(), 1234))
 # s.listen(5)
