278618c97c9591bc2e44254d251574e47566f325
roypyp <roypyp@gmail.com>
2020-12-31 00:24:15 +0200

    dsafadsf

--- Client.py
+++ Client.py
@@ -5,25 +5,28 @@ import sys
 import select
 import tty
 import termios
-import _thread
 import multiprocessing
 
 
 def isData():
     return select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], [])
+
 def test(s2):
     try:
         while 1:
             a=getch.getch()
             s2.sendall(bytes("press","utf-8"))   
     except(KeyboardInterrupt,SystemExit):
-        print("bob")
+        print("InvlidTyping")
+
 print("Client started, listening for offer requests....")
+
+#open a socket for broadcast message
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 #s2= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-s.bind(('',13217))
+s.bind(('',13117))
 
 teamname="Isengard"
 cha=b'\xfe\xed\xbe\xef\x02'
@@ -32,22 +35,20 @@ while True:
         s2= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         message, address = s.recvfrom(10104)
         print( "Received offer from ","172.1.0."+address[0].split(".")[3] ,",attempting to connect...")
+
+        #validation of broadcast message 
         if(message[0:4]!=cha[0:4]):
             print("wrong broadcast")
             continue
+
+        #convert the porn number from hex to decimal
         port_num=int(hex(message[5])[2:]+hex(message[6])[2:],16)
-        print(port_num)
+        #print(port_num)
         s2.connect(("172.1.0."+address[0].split(".")[3], port_num))
         s2.sendall(bytes(teamname,"utf-8"))
         data=s2.recv(10104)
         print(data.decode("utf-8"))
-        '''while True:
-            data=s2.recv(1024)
-            if(data.decode("utf-8")=="mssage"):
-                a=getch.getch()
-                s2.sendall(bytes("press","utf-8"))
-            elif(data.decode("utf-8")=="end"):
-                break'''
+       
         '''starttime=time.time()
         old_settings = termios.tcgetattr(sys.stdin)
         try:
@@ -61,12 +62,13 @@ while True:
                 
         finally:
             termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)'''
-        #_thread.start_new_thread(test,(s2,))
+
         p = multiprocessing.Process(target=test, name="test", args=(s2,))
         p.start()
         time.sleep(10.2)
         p.terminate()
         p.join()
+        
         #test(s2)
         print("Game over!")
         data=s2.recv(10104)
@@ -80,13 +82,3 @@ while True:
 
     
 
-
-# s.connect((socket.gethostname(), 1234))
-# full_msg = ''
-# while True:
-#     msg = s.recv(8)
-#     if len(msg) <= 0:
-#         break
-#     full_msg += msg.decode("utf-8")
-
-# print(full_msg)
--- Server.py
+++ Server.py
@@ -4,6 +4,7 @@ import _thread
 import select
 
 teamsdict={}
+
 def listenT(s):
     global teamsdict
     startTime=time.time()
@@ -14,31 +15,29 @@ def listenT(s):
         conn, addr = s.accept()
         data = conn.recv(1024)
         teamsdict[addr]=[conn,data.decode("utf-8")]
-        #print(data.decode("utf-8"))
-        #_thread.start_new_thread(listenT,())
+      
+
+
 def broadCast():
     s = socket(AF_INET, SOCK_DGRAM)
     s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+
+    #The broadcast message
     msg = b'\xfe\xed\xbe\xef\x02\x25\x13'
     
     for i in range(10):
-    #s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-        s.sendto(msg, ('<broadcast>', 13217))
+        s.sendto(msg, ('<broadcast>', 13117))
         time.sleep(1)
 
 
+
 def gameStart(s,inx,a,flag):
-    
     while True:
         if(flag[0]):
             break
-        #print("!")
-        #s.sendall(bytes("mssage","utf-8"))
         data=s.recv(1024)
-        #if(data.decode("utf-8")=="press"):
         a[inx]+=1
-            #print (a)
-    #s.sendall(bytes("end","utf-8"))
+   
 
 
 def KSBR():
@@ -46,6 +45,8 @@ def KSBR():
     flagG=0
     group1=[]
     group2=[]
+
+    #divide the clients to 2 groups, odd group and even group
     for key in teamsdict.keys():
         if(flagG==0):
             group1.append([key,teamsdict[key][1]])
@@ -53,6 +54,9 @@ def KSBR():
         else:
             group2.append([key,teamsdict[key][1]])
             flagG=0
+
+
+##building the message - what we will print
     mssage="Welcome to Keyboard Spamming Battle Royale.\n"
     mssage+="Group 1:\n==\n"
     for team in group1:
@@ -61,21 +65,28 @@ def KSBR():
     for team in group2:
         mssage+=team[1]+"\n"
     mssage+="Start pressing keys on your keyboard as fast as you can!!"
+
+    #sending the message to everyone
     for key in teamsdict.keys():
         s=teamsdict[key][0]
         s.sendall(bytes(mssage,"utf-8"))
+
+    #counter of the number of pressing 
     a=[0]*len(teamsdict)
+
     inx=0
     flag=[False]
     for key in teamsdict.keys():
         _thread.start_new_thread(gameStart,(teamsdict[key][0],inx,a,flag))
         inx+=1
+
     time.sleep(10)
     flag[0]=True
     time.sleep(0.2)
     group1point=0
     group2point=0
     flagG=0
+
     for i in range(len(a)):
         if(flagG==0):
             group1point+=a[i]
@@ -83,6 +94,7 @@ def KSBR():
         else:
             group2point+=a[i]
             flagG=0
+
     fmssage="Group 1 typed in "+ str(group1point)+" characters. Group 2 typed in " + str(group2point) +" characters.\n"
     if group1point>=group2point:
         fmssage+="Group 1 wins!\n\n"
@@ -96,13 +108,19 @@ def KSBR():
         fmssage+="Congratulations to the winners:\n==\n"
         for team in group2:
             fmssage+=team[1]+"\n"
+
+    #send the message to everyone(clients)
     for key in teamsdict.keys():
         s=teamsdict[key][0]
         s.sendall(bytes(fmssage,"utf-8"))
+
+    #Cant closing the socket until revicing from client
     for key in teamsdict.keys():
         s=teamsdict[key][0]
         s.recv(1024)
         s.close()
+
+
 
 
 s = socket(AF_INET, SOCK_STREAM)
@@ -119,16 +137,3 @@ while True:
     teamsdict={}
 
 
-
-
-
-
-# s.bind((socket.gethostname(), 1234))
-# s.listen(5)
-# while True:
-    
-#     # now our endpoint knows about the OTHER endpoint.
-#     clientsocket, address = s.accept()
-#     print(f"Connection from {address} has been established.")
-#     clientsocket.send(bytes("Hey there!!! sdfdsf","utf-8"))
-#     clientsocket.close()
